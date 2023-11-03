dbe169415829519456b1f69640f432afae9b8b96
JonathanBex <jonathan.soderberg@hotmail.com>
2023-11-03 10:54:48 +0100

    db exception expanded

--- Core/Builders/Database/DatabaseErrorBuilder.cs
+++ Core/Builders/Database/DatabaseErrorBuilder.cs
@@ -6,15 +6,23 @@ namespace JMock.Core.Builders.Database
   public class DatabaseErrorBuilder
   {
     private string? Message;
-
+    private int? ErrorCode;
     public DatabaseErrorBuilder SetExceptionMessage(string? message = null)
     {
       Message = message;
       return this;
     }
 
+    public DatabaseErrorBuilder SetErrorCode(int? errorCode = null)
+    {
+      ErrorCode = ErrorCode;
+      return this;
+    }
+
     public DbException Build()
     {
+      if (string.IsNullOrEmpty(Message)) throw new InvalidDataException("Missing message for exception");
+      if (ErrorCode != null) return new CustomDbException(Message, ErrorCode.Value);
       return new CustomDbException(Message ?? "Error");
     }
   }
--- Core/Database/Exceptions/CustomDbException.cs
+++ Core/Database/Exceptions/CustomDbException.cs
@@ -5,5 +5,6 @@ namespace JMock.Core.Database.Exceptions
   public class CustomDbException : DbException
   {
     public CustomDbException(string message) : base(message) { }
+    public CustomDbException(string message,int errorCode) : base(message, errorCode) { }
   }
 }
